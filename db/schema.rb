2d95998b86674017b1b860c525bcd59ff3be5ae7
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-11-28 16:50:40 -0500

    create jobs table and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128185353) do
+ActiveRecord::Schema.define(version: 20171128214553) do
 
   create_table "boats", force: :cascade do |t|
     t.string "name"
     t.string "location"
+    t.integer "amount"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "description"
+    t.string "origin"
+    t.string "destination"
+    t.integer "cost"
     t.integer "amount"
     t.integer "user_id"
     t.datetime "created_at", null: false
