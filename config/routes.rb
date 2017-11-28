aca32328e1708b35874998f4aa50e29344184bd4
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-11-28 12:14:33 -0500

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-resources :boats  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+resources :boats
+root 'boats#index'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
