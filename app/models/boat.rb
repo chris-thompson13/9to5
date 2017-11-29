5e1198cc072e2f2ef1d16066b7f0fdc206ad6b63
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-11-29 14:41:22 -0500

    add boat name validation

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,6 +1,8 @@
 class Boat < ApplicationRecord
   belongs_to :user
   has_many :jobs
+  validates :name, presence: true
+  validates :name, uniqueness: true
   has_attached_file :avatar, styles: { big: "500x500>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
