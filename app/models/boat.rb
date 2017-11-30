5a455011e73689e9f006161cd23cb6f959bd91ac
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-11-30 16:35:46 -0500

    change select menu

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,6 +3,7 @@ class Boat < ApplicationRecord
   has_and_belongs_to_many :jobs
   validates :name, presence: true
   validates :name, uniqueness: true
-  has_attached_file :avatar, styles: { big: "500x500>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { big: "500x500>", medium: "300x300>", thumb: "100x100>" }, :default_url => "/images/:thumb/no_picture.png"
+
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
