6320ca12d15c98b8dac4be3dba4f23e5c1a8485f
chris-thompson13 <chris@hiassistant.comgit config --global user.email chris@hiassistant.com>
2017-12-01 10:51:33 -0500

    jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,19 +1,56 @@
 class JobsController < ApplicationController
   def index
+    current_user
+    @jobs = Job.all
   end
 
   def show
+    @job = Job.find_by_id(params[:id])
+
   end
 
   def new
-  end
-
-  def edit
+    @job = Job.new(params[:id])
   end
 
   def create
+    @job = current_user.boats.jobs.new(job_params)
+    if @job.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    find_params
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to job_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+      redirect_to root_path
   end
-end
+
+  private
+  def job_params
+    params.require(:job).permit(:name, :location, :amount, :avatar)
+  end
+
+
+
+    def find_params
+    	@boat = Boat.find(params[:id])
+
+  end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 devise_for :users
 resources :boats
 resources :jobs
-root 'boats#index'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'boats#index'
+root 'jobs#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
